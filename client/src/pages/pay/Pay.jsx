3bb9fbed6a7f21b3ffed5bd84356375d2bf5f1b6
Naveen <naveenbaghel5429@gmail.com>
2023-05-15 19:19:20 +0530

    first commit

---
+++ client/src/pages/pay/Pay.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+// import "./Pay.scss";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import newRequest from "../../utils/newRequest";
+import { useParams } from "react-router-dom";
+import CheckoutForm from "../../components/checkoutForm/CheckoutForm";
+
+const stripePromise = loadStripe(
+  "pk_test_51MstQISHv5lqqmgrGs1hEGICmF0xRFsMjRuC65vk9OnWzdzjrsJt5RIcBzMTD1V0GmOaIoSBWONFzyPQFRMyO7qX00ZQ26n4uH"
+);
+
+const Pay = () => {
+  const [clientSecret, setClientSecret] = useState("");
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await newRequest.post(
+          `/orders/create-payment-intent/${id}`
+        );
+        setClientSecret(res.data.clientSecret);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    makeRequest();
+  }, []);
+
+  const appearance = {
+    theme: 'stripe',
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
+  return <div className="pay">
+    {clientSecret && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm />
+        </Elements>
+      )}
+  </div>;
+};
+
+export default Pay;
